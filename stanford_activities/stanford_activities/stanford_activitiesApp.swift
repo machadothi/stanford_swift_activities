9be632fe978f537d141d59925b28de77d98cdf94
Thiago Cunha <thiago.embedded@gmail.com>
2023-09-19 21:24:02 +0200

    first class

---
+++ stanford_activities/stanford_activities/stanford_activitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_activitiesApp.swift
+//  stanford_activities
+//
+//  Created by Thiago Machado Cunha on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct stanford_activitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
