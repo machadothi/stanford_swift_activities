95d19d48097e7f5d2e070fdd40111f19ef8d0e97
Thiago Cunha <thiago.embedded@gmail.com>
2023-09-19 23:18:14 +0200

    finished assignment one

--- stanford_activities/stanford_activities/ContentView.swift
+++ stanford_activities/stanford_activities/ContentView.swift
@@ -8,41 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["🐝", "🐞", "🕷️", "🦆","🫎", "🪰", "🦕", "🦟" ,"🕊️", "🦩", "🦝", "🐿️"  ] // [String] = Array<String>
+    
+    @State var cardCount = 4 // Don't use @State in real code, not a good practice
+    
     var body: some View {
-        let emojis: [String] = ["🐝", "🐞", "🕷️", "🦆" ] // [String] = Array<String>
         VStack{
-            HStack {
-                ForEach(emojis.indices, id: \.self) { index in // ForEach(0..<4, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView{
+                cards
             }
-            .foregroundColor(.orange)
-            
-            HStack {
-                CardView(content: "🐷")
-                CardView(content: "🪿")
-                CardView(content: "🐺")
-                CardView(content: "🦇")
-            }
-            .foregroundColor(.orange)
-            
-            HStack {
-                CardView(content: "🐞")
-                CardView(content: "🦎")
-                CardView(content: "🦑")
-                CardView(content: "🐙")
-            }
-            .foregroundColor(.orange)
-            
-        }.padding()
-        
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
     }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
+            ForEach(0..<cardCount, id: \.self) { index in // ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            }
+        }.foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill") // implicit 'return' cause of function or scope with single instruction
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill") // implicit 'return' cause of function or scope with single instruction
+    }
+        
+
 }
 
 struct CardView: View {
     // @State creates a pointer to the variable allowing it to be modified at the closured 'onTapGesture'
     // since structs can only be modified inside its main scope
-    @State var isFaceUp: Bool = true
+    @State var isFaceUp: Bool = false
     
     let content: String
     
@@ -50,19 +73,20 @@ struct CardView: View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12) // let means constant
             
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-
-            } else {
-                base.fill() // fill is implicitly called
             }
+            .opacity(isFaceUp ? 0 : 1)
             
-        }.onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+            base.opacity(isFaceUp ? 1 : 0)
+            
+        }.onTapGesture(perform: {
             isFaceUp.toggle()
         })
     }
+    
 }
 
 
